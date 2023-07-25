ab523b4109feb348e90547435ce1a4c60a487a1c
Leonardo Fonseca <leonardo@ejcm.com>
2023-07-25 20:09:52 -0300

    User

---
+++ back/src/models/User.js
@@ -0,0 +1,43 @@
+const DataTypes = require("sequelize");
+const sequelize = require("../config/sequelize");
+
+const User = sequelize.define('User', {
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    profilePhoto: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    birthDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    hash: {
+        type: DataTypes.STRING,
+        allowNull: false
+               
+    },
+
+    salt: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+});
+
+module.exports = User;
